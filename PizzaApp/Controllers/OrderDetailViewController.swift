82a61d40ce9fda0c9615ff3190a227aa8e87126e
Tanish Aggarwal <atanish6@gmail.com>
2026-07-16 13:52:18 -0400

    Added order detail view screen

--- PizzaApp/Controllers/OrderDetailViewController.swift
+++ PizzaApp/Controllers/OrderDetailViewController.swift
@@ -9,13 +9,105 @@ import UIKit
 
 class OrderDetailViewController: UIViewController {
 
+    
+    // MARK: - Outlets
+        @IBOutlet var datePicker: UIDatePicker!
+        @IBOutlet var tbAddress: UITextField!
+        @IBOutlet var sizeSegment: UISegmentedControl!
+        
+        @IBOutlet var swPepperoni: UISwitch!
+        @IBOutlet var swBacon: UISwitch!
+        @IBOutlet var swChicken: UISwitch!
+        
+        @IBOutlet var swMushroom: UISwitch!
+        @IBOutlet var swOnion: UISwitch!
+        @IBOutlet var swPeppers: UISwitch!
+        
+        // keeps track of which avatar the user picked - default to avatar1
+        var selectedAvatar: String = "avatar1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Avatar Buttons
+        @IBAction func avatar1Pressed(_ sender: UIButton) {
+            selectedAvatar = "avatar1"
+            showAvatarChosenAlert(name: "Avatar 1")
+        }
+        
+        @IBAction func avatar2Pressed(_ sender: UIButton) {
+            selectedAvatar = "avatar2"
+            showAvatarChosenAlert(name: "Avatar 2")
+        }
+        
+        @IBAction func avatar3Pressed(_ sender: UIButton) {
+            selectedAvatar = "avatar3"
+            showAvatarChosenAlert(name: "Avatar 3")
+        }
+        
+        func showAvatarChosenAlert(name: String) {
+            let alertController = UIAlertController(title: "Avatar Selected", message: name, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true)
+        }
 
+    
+    // MARK: - Save Order
+        @IBAction func saveOrderPressed(_ sender: UIButton) {
+            
+            // format the date picker's Date into a String for the DB
+            let formatter = DateFormatter()
+            formatter.dateStyle = .medium
+            formatter.timeStyle = .short
+            let deliveryDateString = formatter.string(from: datePicker.date)
+            
+            let addressText = tbAddress.text ?? ""
+            
+            // segmented control index: 0 = Small, 1 = Medium, 2 = Large
+            let sizeValue = sizeSegment.selectedSegmentIndex
+            
+            // build meat toppings string - join only the ones that are ON
+            var meats: [String] = []
+            if swPepperoni.isOn { meats.append("Pepperoni") }
+            if swBacon.isOn { meats.append("Bacon") }
+            if swChicken.isOn { meats.append("Chicken") }
+            let meatToppingsString = meats.joined(separator: ", ")
+            
+            // build veg toppings string
+            var vegs: [String] = []
+            if swMushroom.isOn { vegs.append("Mushroom") }
+            if swOnion.isOn { vegs.append("Onion") }
+            if swPeppers.isOn { vegs.append("Peppers") }
+            let vegToppingsString = vegs.joined(separator: ", ")
+            
+            // build the Order object
+            let newOrder = Order()
+            newOrder.initWithData(theRow: 0,
+                                   theDeliveryDate: deliveryDateString,
+                                   theAddress: addressText,
+                                   theSize: sizeValue,
+                                   theMeatToppings: meatToppingsString,
+                                   theVegToppings: vegToppingsString,
+                                   theAvatar: selectedAvatar)
+            
+            // insert into database via AppDelegate
+            let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+            let returnCode = mainDelegate.insertIntoDatabase(order: newOrder)
+            
+            var returnMSG = "Order Saved!"
+            if returnCode == false {
+                returnMSG = "Order Failed to Save"
+            }
+            
+            let alertController = UIAlertController(title: "Save Order", message: returnMSG, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true)
+        }
     /*
     // MARK: - Navigation
 
--- PizzaApp/Controllers/OrdersTableViewController.swift
+++ PizzaApp/Controllers/OrdersTableViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class OrdersTableTableViewController: UITableViewController {
-
+class OrdersTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // grab AppDelegate for db access
+    let mainDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    // table view outlet - connect in storyboard
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mainDelegate.readDataFromDatabase()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -19,63 +25,27 @@ class OrdersTableTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    // refresh again every time screen appears (in case new order was just saved)
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            mainDelegate.readDataFromDatabase()
+            tableView.reloadData()
+        }
+        
+        // MARK: - Table Methods (stubs for now - filled in Step 7)
+        
+        // required by UITableViewDataSource - number of cells
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return mainDelegate.orders.count
+        }
+        
+        // required by UITableViewDataSource - what goes in each cell
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "orderCell") ?? UITableViewCell()
+            return cell
+        }
+    
+    
     /*
     // MARK: - Navigation
 
